2f88fa6d2d7de364f30150ec75a6c44b90cc2ca9
Kirby Campbell <jkirbycampbell@gmail.com>
2018-09-06 20:05:15 +0000

    automatically backed up by learn

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,8 @@ class StaticController < ApplicationController
 
 
   def hello_world
+    #--Implicit doesn't need the below line - Since it looks in Static View Folder Automatically
+    #--and since this method is called hello_world it also finds the hello_world.html.erb file.
      #"hello_world"
   end
 
